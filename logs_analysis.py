3267b0ee92b9b92086a2a28c1e5c58b07144781c
Kevin Michael <biobot.mike@gmail.com>
2019-03-17 17:46:17 +0200

    Add logs analysis file

---
+++ logs_analysis.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Reporting tool that prints out reports based on data in a database"""
+# Questions this tool should answer:
+# 1. What are the most popular three articles of all time?
+# 2. Who are the most popular article authors of all time?
+# 3. On which days did more than 1% of requests lead to errors?
